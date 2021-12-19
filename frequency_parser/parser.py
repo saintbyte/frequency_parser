92cd9e10af2344ca536a045d93768b11fb29ea4b
Saint_Byte <wwwadmin@bk.ru>
2021-12-19 22:12:40 +0300

    first release

--- frequency_parser/parser.py
+++ frequency_parser/parser.py
@@ -1,3 +1,8 @@
+from .constants import NOTHING
+from .constants import SPACE
+from .constants import SUPPORTED_ABBREVIATIONS
+
+
 def parse(parse_str: str) -> int:
     """
     Parse string with frequency and
@@ -7,7 +12,17 @@ def parse(parse_str: str) -> int:
     Result:
         frequency in hz as int
     """
+    frequency = 0
     if not isinstance(parse_str, str):
         ValueError("Must input string")
-
-    return 0
+    parse_str = parse_str.strip()
+    parse_str = parse_str.lower()
+    parse_str = parse_str.replace(SPACE, NOTHING)
+    multiplier = 0
+    for multiplier_str, multiplier_int in SUPPORTED_ABBREVIATIONS:
+        if multiplier_str in parse_str:
+            parse_str = parse_str.replace(multiplier_str, NOTHING)
+            multiplier = multiplier_int
+            break
+    frequency = int(parse_str) * multiplier
+    return frequency
